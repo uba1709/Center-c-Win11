4f636f605faa2c8b5eed31a872eaab6b188c5c5b
uba1709 <130249082+uba1709@users.noreply.github.com>
2026-03-12 16:11:27 +0100

    02

--- main.cpp
+++ main.cpp
@@ -1,40 +1,56 @@
-#include <iostream>
 #include <windows.h>
-#include <winuser.h>
+#include <iostream>
 using namespace std;
 
-const int WINDOW_WIDTH = 2440;
-const int WINDOW_HEIGHT = 1080;
+enum class Operation{
+    weith = 1,
+    normal,
+};
 
-void centerActiceWindow(){
+const int WINDOW_WIDTH = 2440;
+const int WINDOW_HEIGHT = 1280;
+
+void centerActiveWindow(){
     HWND hwnd = GetForegroundWindow();
-    if(hwnd == NULL)
-        cout << "Nie zaleziono aktulanego okna\n";
+    if (hwnd == NULL)
+        return;
 
     int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-    int screenHeight = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
     int x = (screenWidth - WINDOW_WIDTH) / 2;
-    int y = (screenHeight - WINDOW_HEIGHT) /2;
+    int y = (screenHeight - WINDOW_HEIGHT) / 2;
+    SetWindowPos(
+        hwnd,
+        NULL,
+        x,
+        y,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SWP_NOZORDER | SWP_SHOWWINDOW
+    );
+}
+
+int windowSetting(int choice){
+    cout << "choice: ";
+    cin >> choice;
+    cout << "1. 21/9: 2440x1080";
+    cout << "2. 16/10: 1600x900";
+    return choice;
 }
 
 int main(){
-    cout << "Program uruchomiony\n";
-    cout << "Skrot to: CTRL + ALT + C\n";
-
-    if(!RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT, 'c')){
-        cout << "Nie udalo sie zarejesrowac skrotu\n";
+    int choice;
+    if (!RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT, 'V'))
         return 1;
-    }
     MSG msg;
-
-    while(GetMessage(&msg, NULL, 0, 0)){
-        if(msg.message == WM_HOTKEY){
-            if(msg.wParam == 1){
-                centerActiceWindow();
+    while (GetMessage(&msg, NULL, 0, 0)){
+        if (msg.message == WM_HOTKEY){
+            if (msg.wParam == 1){
+                centerActiveWindow();
             }
         }
     }
     UnregisterHotKey(NULL, 1);
-    return 1;
+    return 0;
 }
